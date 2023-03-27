e3081aa24946a5dec5eff4707975eaa09154586d
Anna Prusakova <hanna.prusakova@gmail.com>
2023-03-27 20:04:54 +0300

    add image domain to config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'https://planets-facts-sigma.vercel.app',
+    ]
+  },
   async redirects() {
     return [
       {
